d2dabed230393e30ad31d21158dee637a78baa0f
Julien DUMAY <judumay@student.42.fr>
2019-02-13 11:42:44 +0100

    d00

--- D01/ex09/ssap2.php
+++ D01/ex09/ssap2.php
@@ -1,3 +1,43 @@
 #!/usr/bin/php
 <?php
+	function ft_split($text)
+	{
+		$test = explode(" ", $text);
+		$result = array_filter($test);
+		sort($result);
+		return($result);
+	}
+	if ($argc < 2)
+	{
+		echo "\n";
+		return ;
+	}
+	$elem = array();
+	foreach ($argv as $arg)
+	{
+		if ($arg != $argv[0])
+		{
+			$tab = ft_split($arg);
+			$elem = array_merge($elem, $tab);
+		}
+	}
+	$i = 0;
+	foreach ($elem as $test)
+	{
+		if(is_numeric($test) == TRUE)
+			$numeric[$i++] = $test;
+		else if(ctype_alpha($test) == TRUE)
+			$string[$i++] = $test;
+		else if(ctype_alpha($test) == FALSE && is_numeric($test) == FALSE)
+			$ascii[$i++] = $test;
+	}
+	rsort($numeric, SORT_NUMERIC);
+	sort($string, SORT_NATURAL | SORT_FLAG_CASE);
+	sort($ascii);
+	foreach($string as $element)
+		echo $element . "\n";
+	foreach($numeric as $element)
+		echo $element . "\n";
+	foreach($ascii as $element)
+		echo $element. "\n";
 ?>
