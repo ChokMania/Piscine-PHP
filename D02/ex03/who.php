60d267884de7771a302261567f4cbc3b454ee314
Julien DUMAY <judumay@student.42.fr>
2019-03-08 13:17:08 +0100

    test

--- D02/ex03/who.php
+++ D02/ex03/who.php
@@ -1,20 +1,20 @@
 #!/usr/bin/php
 <?php
-	date_default_timezone_set('Europe/Paris');
-	$file = fopen("/var/run/utmpx", "r");
-	while (!feof($file))
+	$contents = file_get_contents("/var/run/utmpx");
+	$y = [];
+	while ($contents != "")
 	{
-		$data = fread($file, 628);
-		if (strlen($data) == 628)
-		{
-			$data = unpack("a256user/a4id/a32line/ipid/itype/itime", $data);
-			if ($data['type'] == 7)
-			{
-				echo trim($data['user']) . " ";
-				echo trim($data['line']) . "  ";
-				$time = date("M d H:i", $data['time']);
-				echo $time . " \n";
-			}
-		}
+		date_default_timezone_set("Europe/Paris");
+		$x = unpack("A256user/A4id/A32ttyname/ipid/itype/lloginsec/lloginus/A256host/A64pad", $contents);
+		if ($x['type'] == 7)
+			$y[] = $x['user'] . "  " . $x['ttyname'] . "  " . strftime("%b %e %R", $x['loginsec']) . PHP_EOL;
+		$contents = substr($contents, 628);
+	}
+	sort($y);
+	$i = 0;
+	while ($i < count($y))
+	{
+		print($y[$i]);
+		$i++;
 	}
 ?>
