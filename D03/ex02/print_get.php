e6777218ba023f1b25e67b2a501a9d19b66d924b
Julien DUMAY <judumay@student.42.fr>
2019-03-13 14:02:40 +0100

    d03

---
+++ D03/ex02/print_get.php
@@ -0,0 +1,7 @@
+<?php
+	if ($_GET)
+	{
+		foreach($_GET as $i => $val)
+			echo "$i: $val\n";
+	}
+?>
---
+++ D03/ex06/member_only.php
@@ -0,0 +1,9 @@
+<?php
+	if ($_SERVER['PHP_AUTH_USER'] == "zaz" && $_SERVER['PHP_AUTH_PW'] == "jaimelespetitsponeys")
+	{
+		echo "<html><body>\nBonjour Zaz<br />\n<img src='data:image/png;base64,";
+		echo base64_encode(file_get_contents("../img/42.png")) ."'>\n</body></html>\n";
+	}
+	else
+		echo "<html><body>Cette zone est accessible uniquement aux membres du site</body></html>\n";
+?>
