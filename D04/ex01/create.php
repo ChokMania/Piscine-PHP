5d71b11cd3707ad6a2864fee1810d144d4dd7d1b
Julien DUMAY <judumay@student.42.fr>
2019-03-14 10:09:52 +0100

    ex01 + sujet

--- D04/ex01/create.php
+++ D04/ex01/create.php
@@ -4,19 +4,20 @@ if ($_POST["login"] === "" || $_POST["passwd"] === "") {
 	echo "ERROR\n";
 	exit (1);
 }
-if (!mkdir("private"))
-	;
+if (!file_exists("./private"))
+	mkdir("private");
 $login = $_POST["login"];
 $pwd = hash("whirlpool", $_POST["passwd"]);
 $new_user["login"] = $login;
 $new_user["passwd"] = $pwd;
 if (file_exists("./private/passwd"))
 	$users = unserialize(file_get_contents("./private/passwd"));
-foreach($users as $id)
-	if ($id["login"] == $login) {
-		echo "ERROR\n";
-		exit (1);
-	}
+if ($users)
+	foreach($users as $id)
+		if ($id["login"] == $login) {
+			echo "ERROR\n";
+			exit (1);
+		}
 $users[] = $new_user;
 file_put_contents("./private/passwd", serialize($users));
 echo "OK\n";
