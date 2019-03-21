9320562c46cf96771131e6f29991bf928f4d9c24
Julien DUMAY <judumay@student.42.fr>
2019-03-21 17:14:52 +0100

    debut du rush

---
+++ Rush00/rush00/htdocs/index.php
@@ -0,0 +1,29 @@
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<title>Localshop</title>
+		<meta name="description" content="Mini site e-commerce">
+		<link rel="stylesheet" href="css/style.css"/>
+		<link rel="shortcut icon" href="#">
+	</head>
+	<body id="home">
+		<?php include("menu.php"); ?>
+		<article>
+			<h2>Dernieres News du site</h2>
+			<p>Metuentes igitur idem latrones Lycaoniam magna parte campestrem cum se inpares nostris fore congressione stataria documentis frequentibus scirent, tramitibus deviis petivere Pamphyliam diu quidem intactam sed timore populationum et caedium, milite per omnia diffuso propinqua, magnis undique praesidiis conmunitam.</p>
+		</article>
+		<article>
+			<h2>Derniers ajouts</h2>
+			<p>Quibus ita sceleste patratis Paulus cruore perfusus reversusque ad principis castra multos coopertos paene catenis adduxit in squalorem deiectos atque maestitiam, quorum adventu intendebantur eculei uncosque parabat carnifex et tormenta. et ex is proscripti sunt plures actique in exilium alii, non nullos gladii consumpsere poenales. nec enim quisquam facile meminit sub Constantio, ubi susurro tenus haec movebantur, quemquam absolutum.</p>
+			<ul>
+				<li>atque maestitiam</li>
+				<li>atque maestitiam</li>
+				<li>atque maestitiam</li>
+			</ul>
+		</article>
+		<footer>
+			<p>Copyright &copy;2019 Judumay : <a href="https://github.com/ChokMania">Github</a></p>
+		</footer>
+		</div>
+	</body>
+</html>
