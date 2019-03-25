e759109c2d47ffcc63cc21b8c57427b5dc6c757b
Julien DUMAY <judumay@student.42.fr>
2019-03-25 18:23:56 +0100

    push

--- D02/ex03/who.php
+++ D02/ex03/who.php
@@ -1,3 +1,17 @@
 #!/usr/bin/php
 <?php
+	$unpack = "a256user/a4id/A32ttyname/ipid/ltype/ltv/a256host/A64pad";
+	$input = file_get_contents("/var/run/utmpx");
+	while ($input != "")
+	{
+		date_default_timezone_set("Europe/Paris");
+		$result = unpack($unpack, $input);
+		if ($result['type'] == 7)
+		{
+				echo $result['user'] . "  ";
+				echo $result['ttyname'] . "  ";
+				echo strftime("%b %e %H:%M", $result['tv']) . "\n";
+		}
+		$input = substr($input, 628);
+	}
 ?>
