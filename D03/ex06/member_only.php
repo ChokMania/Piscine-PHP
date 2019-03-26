6da68058e68fc1ff8fb2bb7c4103ac5171857090
Julien DUMAY <judumay@student.42.fr>
2019-03-26 17:58:59 +0100

    push

--- D03/ex06/member_only.php
+++ D03/ex06/member_only.php
@@ -1,8 +1,18 @@
 <?php
-	if ($_SERVER['PHP_AUTH_USER'] == "zaz" && $_SERVER['PHP_AUTH_PW'] == "jaimelespetitsponeys") {
-		echo "<html><body>\nBonjour Zaz<br />\n<img src='data:image/png;base64,";
-		echo base64_encode(file_get_contents("../img/42.png")) ."'>\n</body></html>\n";
+	date_default_timezone_set("GMT");
+	$img = "../img/42.png";
+	$users = array('zaz' => 'jaimelespetitsponeys');
+	if ($_SERVER['PHP_AUTH_USER'] != "zaz" || $_SERVER['PHP_AUTH_PW'] != 'jaimelespetitsponeys')
+	{
+		header('WWW-Authenticate: Basic realm="Espace membres"');
+		header('HTTP/1.0 401 Unauthorized');
+		header('date: ' . date('D, d M Y H:i:s T'));
+		echo "<html><body>Cette zone est accessible uniquement aux membres du site</body></html>" . "\n";
 	}
 	else
-		echo "<html><body>Cette zone est accessible uniquement aux membres du site</body></html>\n";
+	{
+		echo "Bonjour Zaz<br />\n<img src='data:image/png;base64,";
+		echo base64_encode($content = file_get_contents($img));
+		echo "'>\n</body></html>\n";
+	}
 ?>
