e7b0f64728308427f6401af4bbd66cf45f1a87c0
Julien DUMAY <judumay@student.42.fr>
2019-03-26 12:26:13 +0100

    maj j01 et j03

---
+++ D01/ex14/agent_stats.php
@@ -0,0 +1,48 @@
+#!/usr/bin/php
+<?php
+	if ($argc < 2 || $argc > 2 || ($argv[1] != "moyenne" &&
+		$argv[1] != "moyenne_user" && $argv[1] != "ecart_moulinette"))
+		exit();
+	$file = file("php://stdin");
+	unset($file[0]);
+	for ($i = 1; $file[$i]; $i++) {
+		$file[$i] = explode(";", $file[$i]);
+		if (strlen($file[$i][1]) == 0)
+			unset($file[$i]);
+	}
+	asort($file);
+	$file = array_values($file);
+	if ($argv[1] == "moyenne")
+	{
+		for ($i = 0; $file[$i]; $i++)
+			if ($file[$i][2] != "moulinette")
+				$moy += $file[$i][1];
+			else
+				$nb++;
+		echo $moy/($i - $nb)."$moy\n";
+	}
+	else
+	{
+		for ($i = 0; $file[$i]; $i++) {
+			if ($file[$i][2] != "moulinette") {
+				$tab1[$file[$i][0]] += $file[$i][1];
+				$tab2[$file[$i][0]]++;
+			}
+		}
+		$tmp = (array_keys($tab1));
+		if ($argv[1] == "moyenne_user")
+			for ($i = 0; $tmp[$i]; $i++) {
+				echo $tmp[$i].":".$tab1[$tmp[$i]]/$tab2[$tmp[$i]]."\n";
+			}
+		else
+		{
+			for ($i = 0; $file[$i]; $i++)
+				if ($file[$i][2] == "moulinette") {
+					$tab3[$file[$i][0]] = $file[$i][1];
+				}
+			for ($i = 0; $tmp[$i]; $i++) {
+				echo $tmp[$i].":".($tab1[$tmp[$i]]/$tab2[$tmp[$i]] - $tab3[$tmp[$i]])."\n";
+			}
+		}
+	}
+?>
--- D03/ex03/cookie_crisp.php
+++ D03/ex03/cookie_crisp.php
@@ -2,7 +2,11 @@
 	if ($_GET["name"] && $_GET["action"] == "set")
 		setcookie($_GET["name"], $_GET["value"], time() + 3600);
 	else if ($_GET["name"] && $_GET["action"] == "get")
-		echo $_COOKIE[$_GET["name"]] . "\n";
+	{
+		echo $_COOKIE[$_GET["name"]];
+		if (strlen($_COOKIE[$_GET["name"]]))
+			echo "\n";
+	}
 	else if ($_GET["name"] && $_GET["action"] == "del")
 		setcookie($_GET["name"], NULL, -1);
 ?>
