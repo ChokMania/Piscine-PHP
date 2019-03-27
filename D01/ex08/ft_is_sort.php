cd43127f870c59b529a0487e6d1edc4ccb083e39
Julien DUMAY <judumay@student.42.fr>
2019-03-27 11:25:37 +0100

    push

--- D01/ex08/ft_is_sort.php
+++ D01/ex08/ft_is_sort.php
@@ -1,13 +1,10 @@
 <?php
 	function	ft_is_sort($tab)
 	{
-		$i = 0;
-		while ($i < count($tab) - 1)
-		{
-			if (strcmp($tab[$i], $tab[$i + 1]) > 0)
-				return (0);
-			$i++;
-		}
-		return (1);
+		$tmp = $tab;
+		sort($tmp);
+		if ($tab == $tmp)
+			return (1);
+		return (0);
 	}
 ?>
