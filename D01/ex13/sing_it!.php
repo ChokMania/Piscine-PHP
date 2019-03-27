287fd6332e91808f75877ccfb4395d100f6fd758
Julien DUMAY <judumay@student.42.fr>
2019-03-27 14:16:41 +0100

    sing it

--- D01/ex13/sing_it!.php
+++ D01/ex13/sing_it!.php
@@ -1,6 +1,5 @@
 #!/usr/bin/php
 <?php
 	if ($argv[1] == "mais pourquoi cette demo ?" && $argc == 2)
-		echo "Tout simplement pour qu'en feuilletant le sujet\non ne 
-			s'apercoive pas de la nature de l'exo\n";
+		echo "Tout simplement pour qu'en feuilletant le sujet\non ne s'apercoive pas de la nature de l'exo..\n";
 ?>
