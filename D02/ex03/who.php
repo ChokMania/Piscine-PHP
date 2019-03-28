b4bc63dcd24cd7fa89bc6f49872646c385c84b59
Julien DUMAY <judumay@student.42.fr>
2019-03-28 19:44:50 +0100

    who

--- D02/ex03/who.php
+++ D02/ex03/who.php
@@ -1,17 +1,14 @@
 #!/usr/bin/php
 <?php
-	$unpack = "a256user/a4id/a32line/ipid/ltype/ltv/a256host/ipad";
-	$input = file_get_contents("/var/run/utmpx");
-	while ($input != "")
-	{
-		date_default_timezone_set("Europe/Paris");
-		$result = unpack($unpack, $input);
-		if ($result['type'] == 7)
-		{
-			echo $result['user'] . "  ";
-			echo $result['line'] . "  ";
-			echo strftime("%b %e %H:%M", $result['tv']) . "\n";
-		}
-		$input = substr($input, 628);
+	$tmp = "a256user/a4id/a32line/ipid/ltype/ltv/a256host/ipad";
+	date_default_timezone_set("Europe/Paris");
+	for ($file = file_get_contents("/var/run/utmpx"); $file != ""; $file = substr($file, 628)) {
+		$unpack = unpack($tmp, $file);
+		if ($unpack["type"] == 7)
+			$tab[] = $unpack["user"]." ".$unpack["line"]."  ".strftime("%b %e %H:%M", $unpack["tv"])."\n";
+	}
+	sort($tab);
+	foreach ($tab as $value) {
+		echo $value;
 	}
 ?>
