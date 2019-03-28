2123672c2cc79f238318ba45d316f21093991e4c
Julien DUMAY <judumay@student.42.fr>
2019-03-28 21:07:35 +0100

    maj

--- D02/ex00/another_world.php
+++ D02/ex00/another_world.php
@@ -1,5 +1,9 @@
 #!/usr/bin/php
 <?php
-	if ($argc == 2)
-		echo preg_replace("/[ \t]+/", " ", trim($argv[1], " \t")) . "\n";
+	if ($argc >= 2)
+	{
+		$tab = preg_replace("/[ \t]+/", " ", trim($argv[1], " \t"));
+		if ($tab || is_numeric($tab))
+			echo $tab . "\n";
+	}
 ?>
