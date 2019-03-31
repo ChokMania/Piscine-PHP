9ccf104e3738cb06c1e10e642c53259899813db9
Julien DUMAY <judumay@student.42.fr>
2019-03-31 08:41:43 +0200

    avant csv

--- Rush00/admin.php
+++ Rush00/admin.php
@@ -1,7 +1,6 @@
 <?php
-session_start();
+	session_start();
 ?>
-
 <html lang="fr">
 	<head>
 		<meta charset="utf-8">
@@ -10,34 +9,35 @@ session_start();
 		<link rel="stylesheet" href="css/style.css"/>
 		<link rel="shortcut icon" href="#">
 		<style>
-		.boxe {
-			position: relative;
-			margin-left: auto;
-			margin-right: auto;
-			background-color: lightgrey;
-			width: 50%;
-			height: 25%;
-			min-width: 330px;
-			margin-top: 20px;
-			border-radius: 10px 10px;
-		}
-		
-		h1 {
-			text-align: center;
-			margin-top: -10px;
-		}
-		form {
-			display: flex;
-			flex-direction: column;
-			align-items: center;
-		}
-		form input{
-			width: 300px;
-		}
-		</style>
+			.boxe {
+				position: relative;
+				margin-left: auto;
+				margin-right: auto;
+				background-color: lightgrey;
+				width: 50%;
+				height: 25%;
+				min-width: 330px;
+				margin-top: 20px;
+				border-radius: 10px 10px;
+			}
+			
+			h1 {
+				text-align: center;
+				margin-top: -10px;
+			}
+			form {
+				display: flex;
+				flex-direction: column;
+				align-items: center;
+			}
+			form input{
+				width: 300px;
+			}
+			</style>
 	</head>
 	<body>
 		<?php include("menu.php"); ?>
+			
 		<div class="boxe">
 			<br>
 			<h1>Utilisateur</h1>
@@ -46,11 +46,12 @@ session_start();
 			<form>
 			<form method="post" action="user.php">
 			<label>Utilisateur à ajouter: </label><input type="text" name="user" placeholder="Entrez Ici" value="">
+			<label>Mot de passe: </label><input type="text" name="user" placeholder="Entrez Ici" value="">
 			<form>
 			<form method="post" action="user.php">
 			<label>Utilisateur: </label><input type="text" name="id" placeholder="Entrez Ici" value="">
-			<label>Old mot de passe: </label><input type="text" name="oldpw" placeholder="Entrez Ici" value="">
-			<label>New mot de passe: </label><input type="text" name="newpw" placeholder="Entrez Ici" value="">
+			<label>Mot de passe a modifer: </label><input type="text" name="passwd" placeholder="Entrez Ici" value="">
+			<a href=""> Liste des Utilisateurs</a>
 			<form>
 		</div>
 		<div class="boxe">
@@ -59,7 +60,18 @@ session_start();
 		</div>
 		<div class="boxe">
 			<br>
-			<h1>Commandes</h1>	
+			<h1>Commandes clients</h1>	
 		</div>
 	</body>
 </html>
+
+
+
+
+<?php
+	include 'function.php';
+	if (!check_root($_SESSION['loggued_on_user'])) {
+		header("Location: index.php");
+		return ;
+	}
+?>
