4d31a3a4090984349ba2a8a0182db22a74e9fa43
Julien DUMAY <judumay@student.42.fr>
2019-04-01 17:43:48 +0200

    abusé

--- D05/ex16/ex16.sql
+++ D05/ex16/ex16.sql
@@ -1,3 +1,4 @@
-SELECT COUNT(DISTINCT `id_film`) AS `films` FROM `member_history`
-WHERE `date` BETWEEN '2006/10/30' AND '2007/07/27'
-OR (EXTRACT(DAY FROM `date`) = 24 AND EXTRACT(MONTH FROM `date`) = 12)
+SELECT COUNT(`date`) AS 'film' FROM `member_history`
+WHERE YEAR(`date`) >= 2006 AND MONTH(`date`) >= 10 AND DAY(`date`) >= 30
+AND YEAR(`date`) <= 2007 AND MONTH(`date`) <= 07 AND DAY(`date`) <= 27
+OR MONTH(`date`) = 12 AND DAY(`date`) = 24;
