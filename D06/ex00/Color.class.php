976f9818c6ccb792fdb487588a209ff29f218a23
Julien DUMAY <judumay@student.42.fr>
2019-04-02 16:03:10 +0200

    maj

--- D06/ex00/Color.class.php
+++ D06/ex00/Color.class.php
@@ -1,61 +1,96 @@
 <?php
-Class Color {
-
+class Color {
+	
 	public $red = 0;
 	public $green = 0;
 	public $blue = 0;
 	public static $verbose = FALSE;
 
-	function __construct( $palette ) {
-		if ($palette['rgb'] != "") {
-			$number = intval($palette['rgb']);
+	function __construct( $kwargs ) {
+		if (isset($kwargs['rgb'])) {
+			$number = intval($kwargs['rgb']);
 			$this->blue = $number & 0xff;
 			$this->green = $number >> 8 & 0xff;
-			$this->red = ($number >> 16) & 0xff;
+			$this->red = $number >> 16 & 0xff;
 		}
 		else {
-			if ($palette['red'] != "")
-				$this->red = intval($palette['red']);
-			if ($palette['blue'] != "")
-				$this->blue = intval($palette['blue']);
-			if ($palette['green'] != "")
-				$this->green = intval($palette['green']);
+			if (isset($kwargs['red']))
+				$this->red = intval($kwargs['red']);
+			if (isset($kwargs['blue']))
+				$this->blue = intval($kwargs['blue']);
+			if (isset($kwargs['green']))
+				$this->green = intval($kwargs['green']);
 		}
-		if (Color::$verbose)
+		if (self::$verbose)
 			printf("Color( red: %3d, green: %3d, blue: %3d ) constructed.\n", $this->red, $this->green, $this->blue);
 	}
 
-	public function __destruct () {
+	function __destruct() {
 		if (self::$verbose)
-			return (printf("Color( red: %3d, green: %3d, blue: %3d ) destructed.\n", $this->red, $this->green, $this->blue));
+			printf("Color( red: %3d, green: %3d, blue: %3d ) destructed.\n", $this->red, $this->green, $this->blue);
 	}
 
-	public function __toString() {
-		return (vsprintf("Color( red: %3d, green: %3d, blue: %3d )",
-			array ($this->red, $this->green, $this->blue)));
+	function __toString() {
+		return sprintf("Color( red: %3d, green: %3d, blue: %3d )", $this->red, $this->green, $this->blue);
 	}
 
-	public static function doc() { return (file_get_contents('Color.doc.txt')); }
+	static function doc() { echo file_get_contents("./Color.doc.txt"); }
 
-	public function add($new_color)
-	{
-		return (new Color(array ('red' => $this->red + $newcolor,
-		'green' => $this->green + $newcolor,
-		'blue' => $this->blue + $newcolor)));
+	function add($color) {
+		$newred = $color->red + $this->red;
+		if ($newred > 255)
+			$newred = 255;
+		else if ($newred < 0)
+			$newred = 0;
+		$newblue = $color->blue + $this->blue;
+		if ($newblue > 255)
+			$newblue = 255;
+		else if ($newblue < 0)
+			$newblue = 0;
+		$newgreen = $color->green + $this->green;
+		if ($newgreen > 255)
+			$newgreen = 255;
+		else if ($newgreen < 0)
+			$newgreen = 0;
+		return new Color(array("red" => $newred, "blue" => $newblue, "green" => $newgreen));
 	}
 
-	public function sub($new_color)
-	{
-		return (new Color(array ('red' => $this->red - $newcolor,
-		'green' => $this->green - $newcolor, 
-		'blue' => $this->blue - $newcolor)));
+	function mult($factor) {
+		$newred = $factor * $this->red;
+		if ($newred > 255)
+			$newred = 255;
+		else if ($newred < 0)
+			$newred = 0;
+		$newblue = $factor * $this->blue;
+		if ($newblue > 255)
+			$newblue = 255;
+		else if ($newblue < 0)
+			$newblue = 0;
+		$newgreen = $factor * $this->green;
+		if ($newgreen > 255)
+			$newgreen = 255;
+		else if ($newgreen < 0)
+			$newgreen = 0;
+		return new Color(array("red" => $newred, "blue" => $newblue, "green" => $newgreen));
 	}
 
-	public function mult($new_color)
-	{
-		return (new Color(array ('red' => $this->red * $newcolor,
-		'green' => $this->green * $newcolor, 
-		'blue' => $this->blue * $newcolor)));
+	function sub($color) {
+		$newred = $this->red - $color->red;
+		if ($newred > 255)
+			$newred = 255;
+		else if ($newred < 0)
+			$newred = 0;
+		$newblue = $this->blue - $color->blue;
+		if ($newblue > 255)
+			$newblue = 255;
+		else if ($newblue < 0)
+			$newblue = 0;
+		$newgreen = $this->green - $color->green;
+		if ($newgreen > 255)
+			$newgreen = 255;
+		else if ($newgreen < 0)
+			$newgreen = 0;
+		return new Color(array("red" => $newred, "blue" => $newblue, "green" => $newgreen));
 	}
 }
 ?>
